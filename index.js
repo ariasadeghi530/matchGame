393ac5535b594b21e3d266021f81e6725dab0c1c
Aria Sadeghi <ariasadeghi@gmail.com>
2020-02-14 12:43:10 -0800

    added logic to store player answers

---
+++ index.js
@@ -0,0 +1,10 @@
+let contestAnswers = [];
+let playerArr =[];
+
+// based on input from inquirer
+for (let i = 0; i < numContestants; i++){
+  playerArr.push({
+    name: `${inputName}`
+    score: 0
+  })
+}
