b3525e0ac51b49a90d50bbc92893d4a5f4aa71f1
Carlo <csnazareno94@gmail.com>
2020-02-14 12:42:58 -0800

    initial questions

---
+++ index.js
@@ -0,0 +1,11 @@
+const prompt = require('inquirer').createPromptModule()
+
+
+let playerNumQues = [
+  'What is the Contestant name?',
+  'How many panelists?'
+]
+
+let questions = [
+  'What is your favorite color?'
+]
