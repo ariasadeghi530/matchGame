af44a2a2b3e93cdc09406d103c690e60309dfb63
Carlo <csnazareno94@gmail.com>
2020-02-14 12:55:00 -0800

    skeleton

--- index.js
+++ index.js
@@ -7,5 +7,24 @@ let playerNumQues = [
 ]
 
 let questions = [
-  'What is your favorite color?'
+  'What is your favorite color?',
+  'What is your favorite movie?',
+  'When is your birthday?',
+  'What is your favorite book?',
+  ''
 ]
+
+prompt({
+  type: 'input',
+  name: 'contestant',
+  message: playerNumQues[0]
+})
+.then ( response => {
+  console.log(response.contestant)
+    prompt({
+      type: 'input',
+      name: 'panelistCount',
+      
+    })
+})
+
